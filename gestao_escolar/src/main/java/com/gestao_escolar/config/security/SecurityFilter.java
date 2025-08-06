0d31a02a28d1d29aff45f7059c80a060d5928b30
LucasRP <rodrigues.lrp21@gmail.com>
2025-08-05 21:45:24 -0300

    feat: add dos módulos de segurança

---
+++ gestao_escolar/src/main/java/com/gestao_escolar/config/security/SecurityFilter.java
@@ -0,0 +1,44 @@
+package com.gestao_escolar.config.security;
+
+import com.gestao_escolar.repository.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recuperarToken(request);
+        if (token != null) {
+            var login  = jwtService.validadeToken(token);
+            UserDetails user = usuarioRepository.findByLogin(login);
+
+            var autenticacao  = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(autenticacao);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var tokenAutorizacao = request.getHeader("Authorization");
+        if (tokenAutorizacao == null) return null;
+        return tokenAutorizacao.replace("Bearer ", "");
+    }
+}
