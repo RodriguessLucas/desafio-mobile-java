81a28a259eb174a363c266b730a248a06736970f
LucasRP <rodrigues.lrp21@gmail.com>
2025-08-09 14:47:06 -0300

    feat: desenvolvimento do endpoint de listar turmas para diretor

--- gestao_escolar/src/main/java/com/gestao_escolar/controller/DiretorController.java
+++ gestao_escolar/src/main/java/com/gestao_escolar/controller/DiretorController.java
@@ -1,7 +1,9 @@
 package com.gestao_escolar.controller;
 
 import com.gestao_escolar.model.dto.ResListProfessorDTO;
+import com.gestao_escolar.model.dto.ResListTurmaDTO;
 import com.gestao_escolar.service.ProfessorMateriaService;
+import com.gestao_escolar.service.TurmaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -19,10 +21,20 @@ public class DiretorController {
     @Autowired
     private ProfessorMateriaService professorMateriaService;
 
+    @Autowired
+    private TurmaService turmaService;
+
     @GetMapping("/professores")
     public ResponseEntity<List<ResListProfessorDTO>> listarProfessores() {
         var listaProfessores = professorMateriaService.listarProfessoresComMaterias();
 
         return ResponseEntity.ok(listaProfessores);
+    }
+
+
+    @GetMapping("/turmas")
+    public ResponseEntity<List<ResListTurmaDTO>> listarTurmas() {
+        var listaTurmas = turmaService.listarTurmas();
+        return ResponseEntity.ok(listaTurmas);
     }
 }
---
+++ gestao_escolar/src/main/java/com/gestao_escolar/service/TurmaService.java
@@ -0,0 +1,22 @@
+package com.gestao_escolar.service;
+
+import com.gestao_escolar.model.dto.ResListTurmaDTO;
+import com.gestao_escolar.repository.TurmaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TurmaService {
+
+    @Autowired
+    private TurmaRepository turmaRepository;
+
+    public List<ResListTurmaDTO> listarTurmas() {
+        var listTurmas = turmaRepository.findAll();
+        return listTurmas.stream().map(ResListTurmaDTO::new).collect(Collectors.toList());
+    }
+
+}
