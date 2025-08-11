3b42297d7e77aba1b4508e5f83dd8fc8d6567392
LucasRP <rodrigues.lrp21@gmail.com>
2025-08-11 01:17:23 -0300

    refactor: ajustes para permitir acesso ao swagger

--- gestao_escolar/src/main/java/com/gestao_escolar/config/doc/OpenApiConfig.java
+++ gestao_escolar/src/main/java/com/gestao_escolar/config/doc/OpenApiConfig.java
@@ -2,13 +2,13 @@ package com.gestao_escolar.config.doc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -32,6 +32,10 @@ public class OpenApiConfig {
                         .title("API de Gestão Escolar")
                         .description("API REST para o sistema de gestão escolar, contendo funcionalidades para alunos, professores e diretores.")
                         .version("v1.0.0")
-                );
+                )
+                .servers(List.of(
+                        new Server().url("https://gestao-escolar-m4yq.onrender.com").description("Servidor de Produção"),
+                        new Server().url("http://localhost:8080").description("Servidor Local de Desenvolvimento")
+                ));
     }
 }
